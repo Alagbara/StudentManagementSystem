9ab4b3f6d4f85c827cf9affc47f7ec1f54251496
github username <github email address>
2019-08-28 12:36:04 +0100

    Models created

---
+++ StudentmanagementSystem/Models/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace StudentmanagementSystem.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string RoleType { get; set; }
+
+
+        public Role()
+        {
+        }
+    }
+}
---
+++ StudentmanagementSystem/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace StudentmanagementSystem.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string PasswordHash { get; set; }
+
+
+        public User()
+        {
+        }
+    }
+}
