7b9a38f789ed6d141f316531bddfcc181fbd1d51
github username <github email address>
2019-08-28 15:30:56 +0100

    migration set up

---
+++ StudentmanagementSystem/ClientApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using StudentmanagementSystem.Models;
+
+namespace StudentmanagementSystem.ClientApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+    }
+}
--- StudentmanagementSystem/Startup.cs
+++ StudentmanagementSystem/Startup.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StudentmanagementSystem.ClientApp.Data;
 
 namespace StudentmanagementSystem
 {
@@ -21,6 +23,8 @@ namespace StudentmanagementSystem
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
