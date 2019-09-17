65c05b401ba3755c4c58580fe0bdfff96ad84182
github username <github email address>
2019-09-17 13:49:20 +0100

    added repository base class

---
+++ StudentmanagementSystem/Models/IRepositoryBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace StudentmanagementSystem.Models
+{
+    public interface IRepositoryBase<T>
+    {
+        IQueryable<T> FindAll();
+        IQueryable<T> FindByCondition(Expression<Func <T, bool>> Expression);
+        void Create(T StudentmanagementSystem);
+        void Update(T StudentmanagementSystem);
+        void Delete(T StudentmanagementSystem);
+    }
+}
